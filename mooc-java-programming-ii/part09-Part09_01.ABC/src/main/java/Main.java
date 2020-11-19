8d69fcb044fb52be6fe45731143a7ec9f76bbf1b
NewbieMcCoder <Mquinter1988@gmail.com>
2020-11-18 20:53:31 -0500

    Section 9

--- mooc-java-programming-ii/part09-Part09_01.ABC/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_01.ABC/src/main/java/Main.java
@@ -3,6 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
         // write your test code here
-        
+        C c = new C();
+        B b = new B();
+        c.a();
+        b.b();
     }
 }
