9a147ddcf9e8310883194799fb9e566d009522ee
NewbieMcCoder <Mquinter1988@gmail.com>
2020-12-07 18:54:11 -0500

    JUnit Challenge

--- IntelliJ IDEA Projects/JUnit_Challenge/src/Main.java
+++ IntelliJ IDEA Projects/JUnit_Challenge/src/Main.java
@@ -2,8 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("App running.");
-        Utilities util = new Utilities();
-        String test = "AABCDDEFF";
-        System.out.println(util.removePairs(test));
     }
 }
--- IntelliJ IDEA Projects/JUnit_Challenge/src/UtilitiesTest.java
+++ IntelliJ IDEA Projects/JUnit_Challenge/src/UtilitiesTest.java
@@ -1,25 +1,51 @@
+import org.junit.Assert;
+
 import static org.junit.Assert.*;
 
 public class UtilitiesTest {
 
+    private Utilities util;
+
+    @org.junit.Before
+    public void setup() {
+        util = new Utilities();
+    }
+
     @org.junit.Test
     public void everyNthChar() {
-        fail("Test is not yet implemented.");
+        char[] array = {'h', 'e', 'l', 'l', 'o'};
+        int n = 2;
+        char[] expectedOutput = {'e', 'l'};
+        assertArrayEquals(expectedOutput, util.everyNthChar(array, n));
+    }
+
+    @org.junit.Test
+    public void everyNthChar_nLargerThanSourceArray() {
+        char[] array = {'h', 'e', 'l', 'l', 'o'};
+        int n = 10;
+        char[] expectedOutput = {'h', 'e', 'l', 'l', 'o'};
+        assertArrayEquals(expectedOutput, util.everyNthChar(array, n));
     }
 
     @org.junit.Test
     public void removePairs() {
-        Utilities util = new Utilities();
         assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
+        assertEquals("ABCABDEF", util.removePairs("ABCCABDEEF"));
     }
 
     @org.junit.Test
     public void converter() {
-        fail("Test is not yet implemented.");
+        assertEquals(300, util.converter(10, 5));
+    }
+
+    @org.junit.Test(expected=ArithmeticException.class)
+    public void converter_arithmeticException() {
+        util.converter(10, 0);
     }
 
     @org.junit.Test
     public void nullIfOddLength() {
-        fail("Test is not yet implemented.");
+        assertNull(util.nullIfOddLength("odd"));
+        assertNotNull(util.nullIfOddLength("even"));
     }
 }
