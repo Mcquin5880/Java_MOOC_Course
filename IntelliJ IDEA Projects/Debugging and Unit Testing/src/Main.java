4e583982a29caa6a3801de76bbb5add1c2cbbf81
NewbieMcCoder <Mquinter1988@gmail.com>
2020-12-07 15:45:34 -0500

    JUnit Challenge

---
+++ IntelliJ IDEA Projects/Debugging and Unit Testing/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("App is running.");
+        BankAccount bankAccount = new BankAccount("Michael", "Quinter", 1000.00, BankAccount.CHECKING);
+
+    }
+}
---
+++ IntelliJ IDEA Projects/JUnit_Challenge/src/UtilitiesTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+public class UtilitiesTest {
+
+    @org.junit.Test
+    public void everyNthChar() {
+        fail("Test is not yet implemented.");
+    }
+
+    @org.junit.Test
+    public void removePairs() {
+        Utilities util = new Utilities();
+        assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
+    }
+
+    @org.junit.Test
+    public void converter() {
+        fail("Test is not yet implemented.");
+    }
+
+    @org.junit.Test
+    public void nullIfOddLength() {
+        fail("Test is not yet implemented.");
+    }
+}
