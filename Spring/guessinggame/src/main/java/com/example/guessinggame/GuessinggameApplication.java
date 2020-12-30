b792cdb33ecd24f9e620088f193249013fdc93dd
NewbieMcCoder <Mquinter1988@gmail.com>
2020-12-29 22:04:00 -0500

    Setup

---
+++ Spring/guessinggame/src/main/java/com/example/guessinggame/GuessinggameApplication.java
@@ -0,0 +1,14 @@
+package com.example.guessinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessinggameApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GuessinggameApplication.class, args);
+	}
+
+}
